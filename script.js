5b563261ade9f9cf9849e4de78f32acb9fa4850d
nawed-code <kohanpoor1379@gamil.com>
2026-05-20 16:10:31 +0200

    gérer play et pause avec js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 "use strict"
 
 console.log("Hi console");
+let lampOn = document.getElementById("lamp"); 
+lampOn.addEventListener("click",function(){
+    
+});
+
+let audio = document.getElementById("audio");
+audio.style.display="none";
+let play = document.getElementById("play");
+play.addEventListener("click",()=>{
+    if(audio.paused){
+        audio.play();
+        play.innerHTML="Pause"
+    }
+    else{
+        audio.pause();
+        play.textContent="Play"
+    }
+});
+
+
+
