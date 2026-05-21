8d46ba10657fe66e45f8948b172a17a85d31bc65
nawed-code <kohanpoor1379@gamil.com>
2026-05-21 09:38:15 +0200

    création de deux fonctions pour la musique précédante et suivante

--- script.js
+++ script.js
@@ -9,16 +9,53 @@ lampOn.addEventListener("click",function(){
 let audio = document.getElementById("audio");
 audio.style.display="none";
 let play = document.getElementById("play");
+let image = document.querySelector("#music img");
+
 play.addEventListener("click",()=>{
     if(audio.paused){
         audio.play();
-        play.innerHTML="Pause"
+        play.innerHTML="Pause";
+        image.classList.toggle("rotate");
     }
     else{
         audio.pause();
-        play.textContent="Play"
+        play.textContent="Play";
+        image.classList.remove("rotate");
     }
 });
+
+
+const songs = [
+    "music/1.mp3",
+    "music/2.mp3",
+    "music/3.mp3"
+
+];
+let back = document.getElementById("back");
+let next = document.getElementById("next");
+let currentSrc = 0;
+audio.setAttribute("src",songs[currentSrc]);
+
+back.addEventListener("click",function(){
+    console.log(audio.getAttribute("src"));
+    currentSrc--;
+    if(currentSrc < 0){
+        currentSrc = songs.length -1;
+    };
+    audio.src = songs[currentSrc];
+    audio.play();
+});
+
+next.addEventListener("click",()=>{
+    currentSrc++;
+    if(currentSrc >= songs.length){
+        currentSrc = 0;
+    };
+    audio.src= songs[currentSrc];
+    audio.play();
+    
+});
+
 
 
 
