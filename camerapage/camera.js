1207bc6faaf27641858896afcc3b24ec172bd679
nawed-code <kohanpoor1379@gamil.com>
2026-05-26 18:50:53 +0200

    création de fichier js et l'objet pieces

---
+++ camerapage/camera.js
@@ -0,0 +1,55 @@
+"use strict"
+
+const pieces = [
+
+    {
+        image : "../image/smart-house.png",
+        title : "Salon",
+        position : "Rez-de-chaussée",
+        superficie : "35m²",
+        portes : 2
+    },
+    {
+        image : "../image/smart-house.png",
+        title : "Cuisine",
+        position : "Rez-de-chaussée",
+        superficie : "18m²",
+        portes : 1
+    },
+    {
+        image : "../image/smart-house.png",
+        title : "Chambre Parentale",
+        position : "1er étage",
+        superficie : "25m²",
+        portes : 1
+    },
+    {
+        image : "../image/smart-house.png",
+        title : "Salon 2",
+        position : "1er étage",
+        superficie : "20m²",
+        portes : 2
+    },
+    {
+        image : "../image/smart-house.png",
+        title : "Garage",
+        position : "Sous-sol",
+        superficie : "28m²",
+        portes : 1
+    },
+    {
+        image : "../image/smart-house.png",
+        title : "Jardin",
+        position : "Extérieur",
+        superficie : "68m²",
+        portes : 3
+    },
+    {
+        image : "../image/smart-house.png",
+        title : "Entrée",
+        position : "Extérieur",
+        superficie : "4m²",
+        portes : 1
+    }
+
+];
