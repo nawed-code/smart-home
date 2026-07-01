006891a387c2b66f6dbfe9c045c30b2e33ca93ae
nawed-code <kohanpoor1379@gamil.com>
2026-07-01 16:50:12 +0200

    Achèvement de la fonction botMessages et connexion du serveur avec Ollama ai

--- chatBot.js
+++ chatBot.js
@@ -1,4 +1,7 @@
 "use strict"
+
+//const { json } = require("body-parser");
+
 console.log("salut depuis chatBot.js");
 let input = document.getElementById("question");
 let textBtn = document.getElementById("send");
@@ -30,14 +33,35 @@ function botMessages(message){
     div.className = "bot-message";
     if(message.trim() === "salut"){
         div.textContent="Salut, je suis Nawed";
+        messages.appendChild(div);
+
     }
-    else if(message.trim() === "tu vas bien"){
-        div.textContent ="Je vais bien, merci";
+    else if(message.trim() === "hi"){
+        div.textContent ="Hi I am Nawed , what can I do for you ?";
+        messages.appendChild(div);
     }
     else{
-        div.textContent ="Je ne comprends pas";
-    }
-    messages.appendChild(div);
 
+        let chatRequete = new XMLHttpRequest();
+        chatRequete.open("POST", "http://localhost:3000/chatbot",true);
+        chatRequete.setRequestHeader("Content-Type","application/json");
+        chatRequete.responseType ="json";
+        chatRequete.addEventListener("load",()=>{
+            if(chatRequete.status == 200){
+                //console.log("hihih");
+                let data = chatRequete.response;
+                //console.log(data.answer);
+                div.textContent = data.answer;
+                messages.appendChild(div);
+            }
+            else{
+                console.log(`erreur : ${chatRequete.status}`);
+            }
+        });
+
+        chatRequete.send(JSON.stringify({
+            question : message
+        }));
+    }
 }
 
